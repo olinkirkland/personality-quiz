ee118f82f02e9f51844302fd004c1e6af118f3c3
Olin Kirkland <me@olinkirk.land>
2023-04-10 14:02:32 +0200

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div className="main">
       <Quiz questions={questions} />
     </div>
   );
